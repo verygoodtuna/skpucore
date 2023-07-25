44d421ad37a222f0800a41076c62e46801dd9d6a
verygoodtuna <kjy990504@gmail.com>
2023-07-25 18:21:36 +0900

    :p

--- js/skpucore.js
+++ js/skpucore.js
@@ -6,4 +6,11 @@ $(function () {
     const SkSlide = new Swiper ('.sk_slide', {
         loop: true,
     })
+
+    $('.product_slide').slick ({
+        arrows: false,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '100px',
+    })
 })
