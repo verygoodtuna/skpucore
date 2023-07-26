331b88fa346e40768b7f21a2e79bce4ba777c28b
verygoodtuna <kjy990504@gmail.com>
2023-07-26 16:41:37 +0900

    .3.

--- js/skpucore.js
+++ js/skpucore.js
@@ -1,6 +1,7 @@
 $(function () {
-    $('.main_box').fullpage({
 
+    $('.main_box').fullpage({
+       
     })
 
     const SkSlide = new Swiper ('.sk_slide', {
@@ -11,6 +12,14 @@ $(function () {
         arrows: false,
         slidesToShow: 1,
         centerMode: true,
-        centerPadding: '100px',
+        centerPadding: '640px',
+        infinite: true,
+    })
+
+    $('.slide_btn .left').on('click', function () {
+        $('.product_slide').slick('slickPrev');
+    })
+    $('.slide_btn .right').on('click', function () {
+        $('.product_slide').slick('slickNext');
     })
 })
